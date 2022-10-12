ab50b969d38ff447f3a7f8ba924cfb6376763448
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2022-10-12 21:07:00 +0300

    Add files via upload

---
+++ firstStepsInPrograming/Radians to degrees/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Radians_to_degrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //градус = радиан * 180 / π. 
+            double radians = double.Parse(Console.ReadLine());
+            Console.WriteLine(radians*180/Math.PI);
+        }
+    }
+}
