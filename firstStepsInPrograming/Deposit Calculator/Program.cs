702fcaf5f8462b295a8c14f04ec46f20a84f83cf
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2022-10-12 21:04:43 +0300

    Add files via upload

---
+++ firstStepsInPrograming/Deposit Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Deposit_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double depositAmount = double.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            percent /= 100;
+            double interest = depositAmount * percent;
+            double monthInterest = interest / 12;
+            double math = depositAmount + (month * monthInterest);
+            Console.WriteLine(math);
+        }
+    }
+}
