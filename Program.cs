0db5a85a37db8d94fec5a5408a5c50feeb1ee42e
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2022-10-16 21:47:58 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace Building
+namespace Combinations
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            int times = 0;
+            for (int first = 0; first <= n; first++)
+            {
+                for (int second = 0; second <= n; second++)
+                {
+                    for (int third = 0; third <= n; third++)
+                    {
+                        if (first + second + third == n)
+                        {
+                            times++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(times);
         }
     }
 }
