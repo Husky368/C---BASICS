ff6283a8cf5cf64ee6acccddaf6c958c47bf77d4
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2022-10-18 16:35:22 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,36 @@
 ﻿using System;
 
-namespace Password_Guess
+namespace Speed_Info
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string a = Console.ReadLine();
-            if (a == "s3cr3t!P@ssw0rd")
+            //•	При скорост до 10(включително) отпечатайте "slow"
+            //•	При скорост над 10 и до 50(включително) отпечатайте "average"
+            //•	При скорост над 50 и до 150(включително) отпечатайте "fast"
+            //•	При скорост над 150 и до 1000(включително) отпечатайте "ultra fast"
+            //•	При по-висока скорост отпечатайте "extremely fast"
+            double a = double.Parse(Console.ReadLine());
+            if (a <= 10)
             {
-                Console.WriteLine("Welcome");
+                Console.WriteLine("slow");
+            }
+            else if (a <= 50)
+            {
+                Console.WriteLine("average");
+            }
+            else if (a <= 150)
+            {
+                Console.WriteLine("fast");
+            }
+            else if (a <= 1000)
+            {
+                Console.WriteLine("ultra fast");
             }
             else
             {
-                Console.WriteLine("Wrong password!");
+                Console.WriteLine("extremely fast");
             }
         }
     }
