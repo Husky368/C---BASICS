f3dbe67511bcdda73dece435dcead94f8a930ce7
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2022-10-20 21:29:12 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,50 @@
 ﻿using System;
 
-namespace Password_Guess
+namespace Operations_between_numbers
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string a = Console.ReadLine();
-            if (a == "s3cr3t!P@ssw0rd")
+            int n = int.Parse(Console.ReadLine());
+            int odd = 0;
+            int even = 0;
+            int number = 0;
+            for (int i = 1; i <= n; i++)
             {
-                Console.WriteLine("Welcome");
+                number += 1;
+                int currentNumber = int.Parse(Console.ReadLine());
+                if (number % 2 == 1)
+                {
+                    odd += currentNumber;
+                }
+                else if (number % 2 == 0)
+                {
+                    even += currentNumber;
+                }
             }
-            else
+            int bigger = 0;
+            int smaller = 0;
+            if (odd < even)
             {
-                Console.WriteLine("Wrong password!");
+                smaller = odd;
+                bigger = even;
+            }
+            else if (odd > even)
+            {
+                smaller = even;
+                bigger = odd;
+            }
+            if (odd == even)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + even);
+            }
+            else if (odd != even)
+            {
+                int diff = bigger - smaller;
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
             }
         }
     }
