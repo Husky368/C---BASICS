5f63c6f9f643a26e14567d4b72171dbb809731ed
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2022-10-20 21:28:08 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,35 +1,20 @@
 ﻿using System;
 
-namespace Train_The_Trainers
+namespace Password_Guess
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int jury = int.Parse(Console.ReadLine());
-            string input = Console.ReadLine();
-            string currentProject;
-            double currentGrade = 0;
-            int numGrades = 0;
-            double gradesSum = 0;
-            double currentGradeSum = 0;
-            while (input != "Finish")
+            string a = Console.ReadLine();
+            if (a == "s3cr3t!P@ssw0rd")
             {
-                currentProject = input;
-                for (int i = 1; i <= jury; i++)
-                {
-                    currentGrade = double.Parse(Console.ReadLine());
-                    numGrades++;
-                    currentGradeSum += currentGrade;
-                    gradesSum += currentGrade;
-                }
-                double avrg = currentGradeSum / jury;
-                Console.WriteLine($"{currentProject} - {avrg:f2}.");
-                currentGradeSum = 0;
-                input = Console.ReadLine();
+                Console.WriteLine("Welcome");
             }
-            double avrgAll = gradesSum / numGrades;
-            Console.WriteLine($"Student's final assessment is {avrgAll:f2}.");
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
         }
     }
 }
