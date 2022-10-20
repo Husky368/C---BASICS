003c6c1c6e53c503b94774367da11b0b5ecc4e61
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2022-10-20 21:27:23 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,44 +1,35 @@
 ﻿using System;
 
-namespace Building
+namespace Train_The_Trainers
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int flat = int.Parse(Console.ReadLine());
-            int rooms = int.Parse(Console.ReadLine());
-            int curruntFlat = flat;
-            while (curruntFlat>0)
+            int jury = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            string currentProject;
+            double currentGrade = 0;
+            int numGrades = 0;
+            double gradesSum = 0;
+            double currentGradeSum = 0;
+            while (input != "Finish")
             {
-                if (curruntFlat==flat)
+                currentProject = input;
+                for (int i = 1; i <= jury; i++)
                 {
-                    for (int i = 0; i < rooms; i++)
-                    {
-                        Console.Write($"L{curruntFlat}{i} ");
-                    }
-                    curruntFlat--;
-                    Console.WriteLine();
+                    currentGrade = double.Parse(Console.ReadLine());
+                    numGrades++;
+                    currentGradeSum += currentGrade;
+                    gradesSum += currentGrade;
                 }
-                if (curruntFlat % 2 == 0)
-                {
-                    for (int i = 0; i < rooms; i++)
-                    {
-                        Console.Write($"O{curruntFlat}{i} ");
-                    }
-                    curruntFlat--;
-                    Console.WriteLine();
-                }
-                if (curruntFlat % 2 == 1)
-                {
-                    for (int i = 0; i < rooms; i++)
-                    {
-                        Console.Write($"A{curruntFlat}{i} ");
-                    }
-                    curruntFlat--;
-                    Console.WriteLine();
-                }
+                double avrg = currentGradeSum / jury;
+                Console.WriteLine($"{currentProject} - {avrg:f2}.");
+                currentGradeSum = 0;
+                input = Console.ReadLine();
             }
+            double avrgAll = gradesSum / numGrades;
+            Console.WriteLine($"Student's final assessment is {avrgAll:f2}.");
         }
     }
 }
