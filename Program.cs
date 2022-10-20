87ef6081b4e4ba587394048a64f87c425e5f6d0a
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2022-10-20 21:29:49 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,50 +1,156 @@
 ﻿using System;
 
-namespace Operations_between_numbers
+namespace Ski_Trip
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            int odd = 0;
-            int even = 0;
-            int number = 0;
-            for (int i = 1; i <= n; i++)
+            int days = int.Parse(Console.ReadLine());
+            string type = Console.ReadLine();
+            string opinion = Console.ReadLine();
+            days -= 1;
+            double cost = 0;
+
+            switch (type)
             {
-                number += 1;
-                int currentNumber = int.Parse(Console.ReadLine());
-                if (number % 2 == 1)
-                {
-                    odd += currentNumber;
-                }
-                else if (number % 2 == 0)
-                {
-                    even += currentNumber;
-                }
-            }
-            int bigger = 0;
-            int smaller = 0;
-            if (odd < even)
-            {
-                smaller = odd;
-                bigger = even;
-            }
-            else if (odd > even)
-            {
-                smaller = even;
-                bigger = odd;
-            }
-            if (odd == even)
-            {
-                Console.WriteLine("Yes");
-                Console.WriteLine("Sum = " + even);
-            }
-            else if (odd != even)
-            {
-                int diff = bigger - smaller;
-                Console.WriteLine("No");
-                Console.WriteLine($"Diff = {diff}");
+                case "apartment":
+                    if (days < 10)
+                    {
+                        cost = days * 25;
+                        cost *= 0.70;
+                        if (opinion=="positive")
+                        {
+                            cost *= 1.25;
+                        }
+                        else
+                        {
+                            cost *= 0.90;
+                        }
+                        Console.WriteLine($"{cost:f2}");
+                        break;
+                    }
+                    else if (days <= 15)
+                    {
+                        cost = days * 25;
+                        cost *= 0.65;
+                        if (opinion == "positive")
+                        {
+                            cost *= 1.25;
+                        }
+                        else
+                        {
+                            cost *= 0.90;
+                        }
+                        Console.WriteLine($"{cost:f2}");
+                        break;
+                    }
+                    else
+                    {
+                        cost = days * 25;
+                        cost *= 0.50;
+                        if (opinion == "positive")
+                        {
+                            cost *= 1.25;
+                        }
+                        else
+                        {
+                            cost *= 0.90;
+                        }
+                        Console.WriteLine($"{cost:f2}");
+                        break;
+                    }
+                case "room for one person":
+                    if (days < 10)
+                    {
+                        cost = days * 18;
+                        if (opinion == "positive")
+                        {
+                            cost *= 1.25;
+                        }
+                        else
+                        {
+                            cost *= 0.90;
+                        }
+                        Console.WriteLine($"{cost:f2}");
+                        break;
+                    }
+                    else if (days <= 15)
+                    {
+                        cost = days * 18;
+                        if (opinion == "positive")
+                        {
+                            cost *= 1.25;
+                        }
+                        else
+                        {
+                            cost *= 0.90;
+                        }
+                        Console.WriteLine($"{cost:f2}");
+                        break;
+                    }
+                    else
+                    {
+                        cost = days * 18;
+                        if (opinion == "positive")
+                        {
+                            cost *= 1.25;
+                        }
+                        else
+                        {
+                            cost *= 0.90;
+                        }
+                        Console.WriteLine($"{cost:f2}");
+                        break;
+                    }
+                case "president apartment":
+                    if (days < 10)
+                    {
+                        cost = days * 35;
+                        cost *= 0.90;
+                        if (opinion == "positive")
+                        {
+                            cost *= 1.25;
+                        }
+                        else
+                        {
+                            cost *= 0.90;
+                        }
+                        Console.WriteLine($"{cost:f2}");
+                        break;
+                    }
+                    else if (days <= 15)
+                    {
+                        cost = days * 35;
+                        cost *= 0.85;
+                        if (opinion == "positive")
+                        {
+                            cost *= 1.25;
+                        }
+                        else
+                        {
+                            cost *= 0.90;
+                        }
+                        Console.WriteLine($"{cost:f2}");
+                        break;
+                    }
+                    else
+                    {
+                        cost = days * 35;
+                        cost *= 0.80;
+                        if (opinion == "positive")
+                        {
+                            cost *= 1.25;
+                        }
+                        else
+                        {
+                            cost *= 0.90;
+                        }
+                        Console.WriteLine($"{cost:f2}");
+                        break;
+                    }
+                default:
+                    break;
             }
         }
     }
