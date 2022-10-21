b5fedd6a6e5ffd7cf1d360b28b7614f531dbceca
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2022-10-21 21:25:26 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,42 +1,14 @@
 ﻿using System;
 
-namespace Exam_Preparation
+namespace Numbers_Divided_by_3_Without_Reminder
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int numGrades = int.Parse(Console.ReadLine());
-            int numNegGrades = 0;
-            double sumGrades = 0;
-            int exercises = 0;
-            string last = "";
-            while (numGrades > numNegGrades)
+            for (int i = 3; i <= 100; i+=3)
             {
-                string currentEx = Console.ReadLine();
-                if (currentEx!="Enough")
-                {
-                    last = currentEx;
-                }
-                if (currentEx == "Enough")
-                {
-                    Console.WriteLine($"Average score: {(sumGrades / exercises):f2}");
-                    Console.WriteLine($"Number of problems: {exercises}");
-                    Console.WriteLine($"Last problem: {last}");
-                    break;
-                }
-                int grade = int.Parse(Console.ReadLine());
-                sumGrades += grade;
-                exercises++;
-                if (grade <= 4)
-                {
-                    numNegGrades++;
-                }
-
-            }
-            if (numNegGrades >= numGrades)
-            {
-                Console.WriteLine($"You need a break, {numNegGrades} poor grades.");
+                Console.WriteLine(i);
             }
         }
     }
