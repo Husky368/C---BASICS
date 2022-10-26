0f2b0485608701b7c87d8b6c8263a6829dc9ee2c
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2022-10-26 21:18:23 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,30 +1,44 @@
 ﻿using System;
 
-namespace Care_of_Puppy
+namespace Tournament_of_Christmas
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int input = int.Parse(Console.ReadLine());
-            double kilos = input * 1000;
-            double eaten = 0;
-            string adopted = Console.ReadLine();
-            while (adopted != "Adopted")
+            double totalSpace = double.Parse(Console.ReadLine());
+            double availbleSpace = totalSpace;
+            string end = Console.ReadLine();
+            int times = 0;
+            int inside = 0;
+            int nomorespace = 0;
+            while (end!="End")
             {
-                double grams = double.Parse(adopted);
-                eaten += grams;
-                adopted = Console.ReadLine();
+                double suitcase = double.Parse(end);
+                times++;
+                if (times==3)
+                {
+                    suitcase *= 1.10;
+                    times = 0;
+                }
+                if (availbleSpace>suitcase)
+                {
+                    availbleSpace -= suitcase;
+                    inside++;
+                }
+                else
+                {
+                    Console.WriteLine("No more space!");
+                    Console.WriteLine($"Statistic: {inside} suitcases loaded.");
+                    nomorespace++;
+                    break;
+                }
+                end = Console.ReadLine();
             }
-            if (eaten <= kilos)
+            if (nomorespace == 0)
             {
-                double left = kilos - eaten;
-                Console.WriteLine($"Food is enough! Leftovers: {left} grams.");
-            }
-            else
-            {
-                double need = eaten - kilos;
-                Console.WriteLine($"Food is not enough. You need {need} grams more.");
+                Console.WriteLine("Congratulations! All suitcases are loaded!");
+                Console.WriteLine($"Statistic: {inside} suitcases loaded.");
             }
         }
     }
