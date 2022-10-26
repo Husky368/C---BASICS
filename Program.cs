ec3703ca8cb80a420c6d4a721feebd338d9272ee
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2022-10-26 21:17:22 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,31 @@
 ﻿using System;
 
-namespace Ages
+namespace Care_of_Puppy
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //((daysInMonth * capsulesCount) * pricePerCapsule)
-            int N = int.Parse(Console.ReadLine());
-            double totalPrice = 0;
-            for (int i = 1; i <= N; i++)
+            int input = int.Parse(Console.ReadLine());
+            double kilos = input * 1000;
+            double eaten = 0;
+            string adopted = Console.ReadLine();
+            while (adopted != "Adopted")
             {
-                float pricePerCapsule = float.Parse(Console.ReadLine());
-                int days = int.Parse(Console.ReadLine());
-                int capsuleCount = int.Parse(Console.ReadLine());
-                float Price = (days * capsuleCount) * pricePerCapsule;
-                totalPrice += Price;
-                Console.WriteLine($"The price for the coffee is: ${Math.Round(Price, 2, MidpointRounding.AwayFromZero):f2}");
+                double grams = double.Parse(adopted);
+                eaten += grams;
+                adopted = Console.ReadLine();
             }
-            Console.WriteLine($"Total: ${Math.Round(totalPrice, 2, MidpointRounding.AwayFromZero):f2}");
+            if (eaten <= kilos)
+            {
+                double left = kilos - eaten;
+                Console.WriteLine($"Food is enough! Leftovers: {left} grams.");
+            }
+            else
+            {
+                double need = eaten - kilos;
+                Console.WriteLine($"Food is not enough. You need {need} grams more.");
+            }
         }
     }
 }
